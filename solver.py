cc25125fd84338dd270e39dbca6e22d8a0822a64
Christopher Sumnicht <csumnicht@berkeley.edu>
2016-02-13 14:40:39 -0800

    Still need to do run, but general outline is appropriate

--- solver.py
+++ solver.py
@@ -30,18 +30,47 @@ def primitive(x):
 #################################################
 
 from mpi4py import MPI
-from heapq import heappush, heappop
 import hashlib
+from Queue import PriorityQueue
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-def move(pos, gen, do):
-    """Combines do_move and gen_move."""
-    return map(lambda m: do(pos, m), gen(pos))
+class GameState:
+    """
+    Wrapper for the idea of a GameState, not needed
+    by the user, just makes things easier for the
+    framework.
+    """
+    def __init__(self, pos, parent):
+        self.pos    = pos
+        self.parent = parent
+
+    def get_hash(self):
+        """
+        Returns the appropriate hash of a given GameState object.
+        Based off of the value of it's position.
+        """
+        return int(hashlib(self.pos).hexdigest(), 16) % size
+
+
+    def expand(self):
+        """
+        Takes the current position and generates the
+        children positions.
+        """
+        # Raw, in other words, not a GameState object.
+        raw_states = map(lambda m: do_moves(pos, m), gen_moves(pos))
+        # Wrapped, in other words, a GameState object.
+        wrapped_states = map(lambda m: GameState(m, rank))
+        return wrapped_states
 
 class Job:
+    """
+    A job is a game state, parent, and also has a priority for placing
+    jobs in a queue for the processes to work on.
+    """
     def __init__(self, state, parent, priority=0):
         self.state = state
         self.parent = parent
@@ -54,42 +83,76 @@ class Job:
         """
         return cmp(self.priority, other.priority)
 
-def distribute(jobs):
-    """ 
-    Given a list of jobs, distributes them to the cor-
-    rect computer.
+class Process:
     """
-    for job in jobs:
-        to = int(hashlib(job.state).hexdigest(), 16) % size
-        comm.send(job, to)
+    Class that defines the behavior what each process should do
+    """
 
-def wait_receive(job):
-    # TODO
-    pass
+    def run(self):
+        """
+        Main loop for each process
+        """
+        # TODO
+        pass
 
-# The transposition table of resolved states.
-resolved = {}
+    def __init__(self, rank):
+        self.rank = rank
+        self.work = PriorityQueue()
+        self.resolved = {}
+        # Keep a list of sent requests, and received requests,
+        # if sending fails, should be able to handle it some-
+        # how.
+        # As for recieving, should test them when appropriate
+        # in the run loop.
+        self.sent = []
+        self.received = []
+        # Main process will terminate everyone by bcasting the value of
+        # finished to True.
+        self.finished = False
 
-work = []
+    def add_job(self, job):
+        self.work.put(job)
 
-job = None
-update_job = True
+    def lookup(self, game_state):
+        """
+        Takes a GameState object and determines if it is in the 
+        resolved list. Returns the result if this is the case, None
+        otherwise.
+        """
+        return resolved[game_state.pos]
 
-while True:
-    if update_job:
-        job = heappush(work)
-    # Nothing to do.
-    if job == None:
-        continue
-    # Look up
-    if job.state in resolved:
-        send_back(job, resolved)
-    elif isinstance(job.state, list):
-        # Wait to recv job results.
-        update_job = wait_receive(job.state)
-    else:
-        children = move(job, gen_moves, do_move)
-        distribute(children)
-        new_job = Job(children, rank)
+    def distribute(self, game_state):
+        """
+        Given a gamestate distributes the results to the appropriate
+        children.
+        """
+        children = game_state.expand()
+        # Keep a list of the requests made by isend. Something may
+        # fail, so we will need to worry about error checking at
+        # some point.
+        for child in children:
+            self.sent.append(comm.isend(child, dest = child.get_hash()))
+
+    def check_for_updates(self):
+        """
+        Checks if there is new data from other Processes that needs to
+        be received and prepares to recieve it if there is any new data.
+        Returns True if there is new data to be recieved.
+        Returns None if there is nothing to be recieved.
+        """
+        # Probe for any sources
+        if comm.iprobe(source=comm.MPI_ANY_SOURCE):
+            # If there are sources recieve them.
+            self.received.append(comm.irecv(source=comm.MPI_ANY_SOURCE))
+            return True
+        return None
+
+process = Process(rank)
+if process.rank == 0:
+    initial_gamestate = GameState(initial_position, process.rank)
+    initial_job = Job(process.rank, initial_gamestate)
+    process.add_job(initial_job)
+
+process.run()
 
 comm.Barrier()
