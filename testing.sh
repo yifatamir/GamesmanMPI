02216fe149774751d32fc7a4f41a5e6a34751ee0
Scott Werwath <swerwath@berkeley.edu>
2016-03-06 16:49:44 -0800

    Updated testing script

--- testing.sh
+++ testing.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-for i in `seq 2 12`;
+for i in `seq $2 $3`;
   do
+    echo Testing with $i processes
     time mpiexec -n $i python solver_launcher.py $1
   done
-echo Done testing distributed solver, moving to local
-time python solve_local.py $1
+echo Done testing distributed solver
+
+if [ "$4" = "-l" ]; then
+  echo Testing with local, non-distributed solver
+  time python solve_local.py $1
+fi
