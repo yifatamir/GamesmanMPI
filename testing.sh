0d51c408a200a7b0bdd390baa7edc3bbaf66f035
Scott Werwath <swerwath@berkeley.edu>
2016-03-07 23:51:39 -0800

    Fixed testing script

--- testing.sh
+++ testing.sh
@@ -2,18 +2,11 @@
 echo Beginning testing for $1 from $2 to $3, $4 tests each > tests/time_results.txt
 echo Beginning testing for $1 from $2 to $3, $4 tests each> tests/solve_results.txt
 TOTAL=`expr $3 - $2`
-TOTAL=`expr $TOTAL \* $4`
+echo -ne "0%\r"
 for i in `seq $2 $3`;
   do
-    step=`expr $i - $2`
-    step=`expr $step \* $4`
-    for j in `seq 0 $4`
+    for j in `seq 1 $4`
       do
-        step=`expr $step + $j`
-        sp=`expr $step \* 100`
-        percent=$((sp / TOTAL))
-        pc=$((percent / 5))
-        echo -ne "$percent%\r"
         echo -ne "\n" >> tests/time_results.txt
         echo -ne "\n" >> tests/solve_results.txt
         echo Testing with $i processes >> tests/time_results.txt
@@ -26,6 +19,10 @@ for i in `seq $2 $3`;
           (time mpiexec -n $i python solver_launcher.py $1) >> tests/solve_results.txt 2>> tests/time_results.txt
         fi
       done
+      step=`expr $i - $2`
+      sp=`expr $step \* 100`
+      percent=$((sp / TOTAL))
+      echo -ne "$percent%\r"
   done
 echo Done testing distributed solver
 
