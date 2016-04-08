4f97f3d67a3210469d17291215d7c3f9ca6e8a9e
Steven Kobori <skobori@airbears2-10-142-33-98.airbears2.1918.berkeley.edu>
2016-04-08 14:46:04 -0700

    Fixed remoteness bug. Added __str__ function for gamestate so they print nicely

--- src/process.py
+++ src/process.py
@@ -234,10 +234,15 @@ class Process:
         if rem2 is None:
             return GameState(None, rem1.remoteness, rem1.state)
 
-        if rem1.state == WIN or rem2.state == WIN:
+        if rem1.state == LOSS or rem2.state == LOSS:
+            if rem1.state == LOSS and rem2.state == WIN:
+                return GameState(None, rem1.remoteness, LOSS)
+            elif rem1.state == WIN and rem2.state == LOSS:
+                return GameState(None, rem2.remoteness, LOSS)
+            else:
+                return GameState(None, min(rem1.remoteness, rem2.remoteness), LOSS)
+        elif rem2.state == WIN and rem1.state == WIN:
             return GameState(None, max(rem1.remoteness, rem2.remoteness), WIN)
-        elif rem2.state == LOSS and rem1.state == LOSS:
-            return GameState(None, min(rem1.remoteness, rem2.remoteness), LOSS)
         else:
             # Use rem1.state by default, but rem2.state should work too.
             return GameState(None, max(rem1.remoteness, rem2.remoteness), rem1.state)
